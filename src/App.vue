42f7f43c5c04a02e58c2866821a2b7501db42777
赵宝 <zhaobao@lubanec.com>
2017-05-17 17:03:05 +0800

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div id="app">
+    <v-header></v-header>
+    <router-view></router-view>
+  </div>
+</template>
+
+<script type="text/ecmascript-6">
+//  引入组件
+import header from './components/Header.vue'
+
+export default {
+  name: 'app',
+  components: {
+    'v-header': header
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
